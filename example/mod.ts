518fb884e88930a4dd5ac085ec4120fa7e64f2e2
Lukas <69743171+quick007@users.noreply.github.com>
2023-06-25 14:27:31 -0700

    try dgram

--- example/mod.ts
+++ example/mod.ts
@@ -1,4 +1,5 @@
 import options from "../options.json" assert { type: "json" }
+import * as dgram from "https://deno.land/std@0.170.0/node/dgram.ts";
 
 const encoder = new TextEncoder();
 const decoder = new TextDecoder();
@@ -7,10 +8,19 @@ const addr = {
 	port: options.telloport,
 	hostname: options.telloip
 } as const
-const socket = Deno.listenDatagram(addr);
 
-for await (const data of socket) {
-	console.log(decoder.decode(data[0]))
-}
+const socket = dgram.createSocket({ type: "udp4" })
 
-socket.send(encoder.encode("command"), addr)
+socket.bind(8890, options.telloip);
+
+socket.on("message", (msg) => console.log(msg))
+
+socket.send(encoder.encode("command"), options.telloport, options.telloip)
+
+// const socket = Deno.listenDatagram(addr);
+
+// for await (const data of socket) {
+// 	console.log(decoder.decode(data[0]))
+// }
+
+// socket.send(encoder.encode("command"), addr)
