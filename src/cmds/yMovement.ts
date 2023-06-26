d3fec7392c906a43f5e7ab7f40d4ac22482aa66f
Lukas <69743171+quick007@users.noreply.github.com>
2023-06-25 23:00:33 -0700

    finish movement commands

--- src/cmds/yMovement.ts
+++ src/cmds/yMovement.ts
@@ -1,16 +1,15 @@
 import { encode } from "../lib/text.ts";
 import DroneController from "../mod.ts";
 
-let height = 20;
-
+/**
+ * Move your drone on the y axis (up or down)
+ * @param move Move up (+) or move down (-)
+ */
 export function yMovement(this: DroneController, move: number) {
-	const finalHeight = move + height;
-	if (500 < finalHeight || finalHeight < 20) {
-		throw new Error(
-			"The final height value must be between or equal to 20 and 500",
-		);
+	if (500 < move || move < 20) {
+		throw new Error("Move values must be between or equal to 20 and 500");
 	}
-	const to = height >= move ? `down ${move * -1}` : `up ${move}`;
+	const to = move >= 0 ? `up ${move}` : `down ${move * -1}`;
 	return () =>
 		this.socket.send(encode(to), this.options.telloPort, this.options.telloIP);
 }
--- src/cmds/zMovement.ts
+++ src/cmds/zMovement.ts
@@ -2,14 +2,14 @@ import { encode } from "../lib/text.ts";
 import DroneController from "../mod.ts";
 
 /**
- * Move your drone on the x axis (forwards or backwards)
- * @param move Move forward (+) or move backwards (-)
+ * Move your drone on the x axis (left or right)
+ * @param move Move left (+) or move right (-)
  */
-export function xMovement(this: DroneController, move: number) {
+export function zMovement(this: DroneController, move: number) {
 	if (500 < move || move < 20) {
 		throw new Error("Move values must be between or equal to 20 and 500");
 	}
-	const to = move >= 0 ? `forward ${move}` : `back ${move * -1}`;
+	const to = move >= 0 ? `left ${move}` : `right ${move * -1}`;
 	return () =>
 		this.socket.send(encode(to), this.options.telloPort, this.options.telloIP);
 }
