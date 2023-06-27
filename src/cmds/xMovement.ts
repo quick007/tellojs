c6ff9839ae80e8a0b410ce408f90f6e1127cdd4f
Lukas <69743171+quick007@users.noreply.github.com>
2023-06-26 22:10:01 -0700

    make negitives work properly with movement

--- src/cmds/xMovement.ts
+++ src/cmds/xMovement.ts
@@ -6,10 +6,11 @@ import DroneController from "../mod.ts";
  * @param move Move forward (+) or move backwards (-)
  */
 export function xMovement(this: DroneController, move: number) {
-	if (500 < move || move < 20) {
+	const m = move > 0 ? move : move * -1
+	if (500 < m || m < 20) {
 		throw new Error("Move values must be between or equal to 20 and 500");
 	}
-	const to = move >= 0 ? `forward ${move}` : `back ${move * -1}`;
+	const to = move >= 0 ? `forward ${m}` : `back ${m}`;
 	return () =>
 		this.socket.send(encode(to), this.options.telloPort, this.options.telloIP);
 }
--- src/cmds/zMovement.ts
+++ src/cmds/zMovement.ts
@@ -6,10 +6,11 @@ import DroneController from "../mod.ts";
  * @param move Move left (+) or move right (-)
  */
 export function zMovement(this: DroneController, move: number) {
-	if (500 < move || move < 20) {
+	const m = move > 0 ? move : move * -1
+	if (500 < m || m < 20) {
 		throw new Error("Move values must be between or equal to 20 and 500");
 	}
-	const to = move >= 0 ? `left ${move}` : `right ${move * -1}`;
+	const to = move >= 0 ? `left ${m}` : `right ${m}`;
 	return () =>
 		this.socket.send(encode(to), this.options.telloPort, this.options.telloIP);
 }
